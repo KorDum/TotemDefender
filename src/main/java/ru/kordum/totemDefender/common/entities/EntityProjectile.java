5e035045f39074ae6b299efbd43e7db2cdff1564
Alexander Kornilov <alexander.kornilov@yumalabs.ru>
2017-07-06 10:55:37 +0300

    Fix crash

--- src/main/java/ru/kordum/totemDefender/common/entities/EntityProjectile.java
+++ src/main/java/ru/kordum/totemDefender/common/entities/EntityProjectile.java
@@ -36,7 +36,9 @@ public class EntityProjectile extends EntityThrowable {
     @Override
     protected void onImpact(RayTraceResult traceResult) {
         if (!world.isRemote) {
-            if (traceResult.entityHit != null && traceResult.entityHit instanceof EntityLivingBase) {
+            if (owner != null &&
+                traceResult.entityHit != null &&
+                traceResult.entityHit instanceof EntityLivingBase) {
                 EntityLivingBase entity = (EntityLivingBase) traceResult.entityHit;
                 owner.attack(entity);
             }
