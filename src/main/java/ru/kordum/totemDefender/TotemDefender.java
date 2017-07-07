141c939d53e49f5d837b73c1a9e9c723fcf9d615
Alexander Kornilov <alexander.kornilov@yumalabs.ru>
2017-07-07 10:53:47 +0300

    Added de_DE lang

--- src/main/java/ru/kordum/totemDefender/TotemDefender.java
+++ src/main/java/ru/kordum/totemDefender/TotemDefender.java
@@ -28,7 +28,7 @@ import ru.kordum.totemDefender.common.handlers.GuiHandler;
 public class TotemDefender {
     public static final String MODID = "TotemDefender";
     public static final String NAME = "Totem Defender";
-    public static final String VERSION = "1.2.3";
+    public static final String VERSION = "1.2.4";
 
     @SidedProxy(
         clientSide = "ru.kordum.totemDefender.client.ClientProxy",
